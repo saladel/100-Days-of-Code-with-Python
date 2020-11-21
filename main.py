c99fd57371b934487c24aa6ccf89482ca5003fd7
Adewale Sanusi <adewalesanusi.a@gmail.com>
2020-11-21 14:23:23 +0000

    commit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # This is the begining og my 100 days of code with Python
 
-# Day 1: Learnt about the input() function, python variables and naming variables.
+# Day 1: Learnt about the input() and len() function, python variables and naming variables.
 
 # Day 2: 
